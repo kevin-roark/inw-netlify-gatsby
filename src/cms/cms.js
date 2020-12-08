e7262507761dfd126c4b0e9ae2bf5e3d473834ea
Kevin Roark <kroark@brooklynnets.com>
2020-12-07 19:31:12 -0500

    attempt to add radio-archive to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,16 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import RadioArchivePreview from './preview-templates/RadioArchivePreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('radio-archive', RadioArchivePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
